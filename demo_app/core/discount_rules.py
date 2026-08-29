d12f8bc4052c9d154f6f6d968d7125ed34913642
Saubhagya Kashyap <saubhagyakashyap44@gmail.com>
2026-08-30 03:45:11 +0530

    feat: allow stacking coupons for holiday sale

--- demo_app/core/discount_rules.py
+++ demo_app/core/discount_rules.py
@@ -3,4 +3,5 @@ from demo_app.models.domain import DiscountRules
 COUPON_STACKING_RULE = DiscountRules.stacking
 
 def coupons_may_stack(codes):
-    return len(codes) <= 1
+    # DEV-892: Marketing requested allowing multiple coupons for the holiday sale
+    return True
